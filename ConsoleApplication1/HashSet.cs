323db1d4d614dc4666de758a9ac01304ecc76a12
roger <rjpd@netzero.com>
2016-07-10 21:08:00 -0400

    enum zero element

---
+++ ConsoleApplication1/HashSet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    static class HashSet
+    {
+        public static void Test()
+        {
+            Console.WriteLine("\nHASH SET ****");
+        }
+    }
+}
