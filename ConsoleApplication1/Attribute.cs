26ca8b87585c95a54f0818699ad849d7ade248d6
roger <rjpd@netzero.com>
2016-08-28 15:12:48 -0400

    attribute, comment

--- ConsoleApplication1/Attribute.cs
+++ ConsoleApplication1/Attribute.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 namespace ConsoleApplication1
 {
     //named properties allowed in contstructor, but only for attributes
+    //  ie: ctor defined with 1 param: ctor(int n), but can call like this:
+    //  ctor(3, Prop1="abc", Prop2="def"   the props must be in the class, but
+    //  they are not in the ctor params
     class Attribute
     {
     }
