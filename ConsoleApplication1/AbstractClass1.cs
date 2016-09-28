8d3e875bd40b4f47f22eba7b6c255f8c73fd8988
roger <rjpd@netzero.com>
2016-09-28 00:30:52 -0400

    Enumeralbe.Range(), more

---
+++ ConsoleApplication1/AbstractClass1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * - can contain implementation(code body) (but, interface cant)
+ */
+
+namespace ConsoleApplication1
+{
+    class AbstractClass1
+    {
+    }
+}
