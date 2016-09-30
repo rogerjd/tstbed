f00eb6c947f673ca1662227a8b0022636cf61049
roger <rjpd@netzero.com>
2016-09-30 15:08:52 -0400

    lambda

--- ConsoleApplication1/Lambda.cs
+++ ConsoleApplication1/Lambda.cs
@@ -21,5 +21,29 @@ namespace ConsoleApplication1
      */
     static class Lambda
     {
+        public static void Test()
+        {
+            PassAsParam();
+        }
+
+        private static void PassAsParam()
+        {
+            Write(() => "message: " + GetMsgTxt());
+        }
+
+        private static string GetMsgTxt()
+        {
+            return "msg test";
+        }
+
+        //once the func get here, it can decide (bool) whether it gets executed or not
+        //  perhaps, conditional compile, but at runtime, and dont also want params to exec
+        static void Write(Func<string> msg)
+        {
+            if (true) //exec func and params
+            {
+                Utils.WriteDetailLine(msg());
+            }
+        }
     }
 }
