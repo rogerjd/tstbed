202f80d6c57c8ee8e9653ff6bac258536be01c33
roger <rjpd@netzero.com>
2016-10-08 03:54:06 -0400

    closure, abstract

---
+++ ConsoleApplication1/AbstractClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * - can contain implementation(code body) (but, interface cant)
+ */
+
+namespace ConsoleApplication1
+{
+    abstract class AbstractClass
+    {
+        public void Test()
+        {
+            //can have body
+        }
+
+        public abstract void Test1();  //no body if declared abstract
+    }
+}
