ba5bece1417805875be0dac33e0b7462fd19b451
roger <rjpd@netzero.com>
2017-09-24 16:30:53 -0400

    Enumerator

--- ConsoleApplication1/Collections/IEnumerable.cs
+++ ConsoleApplication1/Collections/IEnumerable.cs
@@ -11,6 +11,26 @@ namespace ConsoleApplication1.Collections
     {
         IEnumerator IEnumerable.GetEnumerator()
         {
+            return GetEnumerable();
+        }
+
+        IEnumerator GetEnumerable()
+        {
+            return new MyEnum();
+        }
+    }
+
+    class MyEnum : IEnumerator
+    {
+        public object Current => throw new NotImplementedException();
+
+        public bool MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
             throw new NotImplementedException();
         }
     }
