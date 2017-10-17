c1e670c764510e5c0949c743dbb8658abadd51da
roger <rjpd@netzero.com>
2017-10-17 01:37:08 -0400

    Enum ToStr

--- ConsoleApplication1/Enum.cs
+++ ConsoleApplication1/Enum.cs
@@ -41,14 +41,46 @@ namespace ConsoleApplication1
             CastToEnum();
             EnumDefinedValid();
             PassAsParam(MyDaysOfWeek.Mon);
-            GetLongNamesArray();
+            GetLongNamesArrayToStr();
         }
 
-        private static void GetLongNamesArray()
+        private static void GetLongNamesArrayToStr()
         {
             //todo: array or attribute?
 
+            248
+down vote
+I do this with extension methods:
+
+public enum ErrorLevel
+        {
+            None,
+            Low,
+            High,
+            SoylentGreen
         }
+
+        public static class ErrorLevelExtensions
+        {
+            public static string ToFriendlyString(this ErrorLevel me)
+            {
+                switch (me)
+                {
+                    case ErrorLevel.None:
+                        return "Everything is OK";
+                    case ErrorLevel.Low:
+                        return "SNAFU, if you know what I mean.";
+                    case ErrorLevel.High:
+                        return "Reaching TARFU levels";
+                    case ErrorLevel.SoylentGreen:
+                        return "ITS PEOPLE!!!!";
+                    default:
+                        return "Get your damn dirty hands off me you FILTHY APE!";
+                }
+            }
+        }
+
+    }
 
         private static void PassAsParam(MyDaysOfWeek md)
         {
