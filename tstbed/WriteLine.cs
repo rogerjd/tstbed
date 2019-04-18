000c53c2051a1b9ff59d4e147fe5a385cadc2d63
rjpd@netzero.com <rogerjd>
2019-04-17 21:52:59 -0400

    comments

--- tstbed/WriteLine.cs
+++ tstbed/WriteLine.cs
@@ -11,8 +11,10 @@ namespace tstbed
     {
         static public void Test()
         {
+                                                                            //params
             Console.WriteLine("first and last numbers are: {0} and {7}", 1, 2, 3, 4, 5, 6, 7, 8);
             var n = 55;
+                                    //string interpolation
             Console.WriteLine($"the value of n is: {n}");
         }
     }
