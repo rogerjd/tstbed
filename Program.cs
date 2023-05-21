d6f0325950e114b75054f63ce1e9a6cb0ad9beeb
roger <rjpd@netzero.com>
2023-05-21 19:39:22 -0400

    stringtst

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 ﻿using tstbed.Collections;
 using tstbed.Directoy_File_Path;
+using tstbed;
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!, test bed");
 
 
 List.Test();
+StringTst.Test();
 DictionaryTester.Tester();
 
 IO.Test();
---
+++ StringTst.cs
@@ -0,0 +1,22 @@
+namespace tstbed
+{
+
+    public static class StringTst
+    {
+        public static void Test()
+        {
+            Utils.WriteTopic("string tester");            
+        }
+
+        //concat
+        //substring
+        //index
+        //contains
+        //format
+        //to int
+        //substring
+        //insert
+
+    }
+}
+
