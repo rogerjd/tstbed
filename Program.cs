fec1098e28d02a428e1e1e5d7db6e4a5b0545117
roger <rjpd@netzero.com>
2023-05-22 19:32:19 -0400

    string.Insert

--- Program.cs
+++ Program.cs
@@ -18,3 +18,46 @@ HashSetTst.Test();
 
 new tstbed.Delegates();
 
+
+/*
+Yes, there is a default namespace for the Program.cs file. It is the global namespace, and it is present in every C# file. The global namespace contains all of the types that are not declared in a named namespace. Any identifier in the global namespace is available for use in a named namespace.
+
+The following is an example of a Program.cs file that uses the global namespace:
+
+C#
+using System;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
+Use code with caution. Learn more
+In this example, the Program class is declared in the MyNamespace namespace. However, the Console class is declared in the global namespace. Therefore, the Console.WriteLine() method can be called from the Program class without specifying the namespace.
+
+If you want to avoid using the global namespace, you can declare your own namespace for your types. For example, the following is an example of a Program.cs file that declares its own namespace:
+
+C#
+namespace MyProject
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
+Use code with caution. Learn more
+In this example, the Program class is declared in the MyProject namespace. Therefore, the Console class must be qualified with the namespace when it is used. For example, the following code would print "Hello, world!" to the console:
+
+C#
+MyProject.Console.WriteLine("Hello, world!");
+*/
--- StringTst.cs
+++ StringTst.cs
@@ -5,10 +5,16 @@ namespace tstbed
     {
         public static void Test()
         {
-            Utils.WriteTopic("string tester");            
+            Utils.WriteTopic("string tester");
+
+            //concat
+            Concatenate(); //string interpolation
+
+            Insert();
         }
 
-        //concat
+
+
         //substring
         //index
         //contains
@@ -17,6 +23,47 @@ namespace tstbed
         //substring
         //insert
 
+        /*
+            AssignmentByValImmutable();
+            Contains();
+            StartsWith();
+            EndsWith();
+            IndexOf();
+            SubStr();
+            Compare();
+            Concatenate();
+            Replace();
+            SplitIntoArrayJoin();
+            Interpolation();
+            EmptyNullWhitespace();
+            Escape();
+            //Trim TrimStart TrimEnd
+            Trim();
+
+            //Remove  delete
+            //Join
+            //Split
+            //Length property
+            Format();
+            Equals();
+        */
+        private static void Concatenate()
+        {
+            Console.WriteLine("concat");
+            string s1 = "abc";
+            string s2 = "def";
+            Console.WriteLine($"{s1} + {s2} = {s1 + s2}");
+        }
+
+        static void Insert()
+        {
+            Console.WriteLine("insert");
+            string s1 = "abc";
+            string s2 = "Z";
+            string s3 = s1.Insert(1, s2);
+            Console.WriteLine($" Insert {s2} in {s1} at pos 1 = {s3}");
+
+        }
     }
 }
 
