6b01574156403c2eedeca397002e69e1706db998
roger <rjpd@netzero.com>
2024-09-18 20:35:10 -0400

    ProcessTester and fmt

---
+++ ProcessTst.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+
+namespace tstbed
+{
+
+    public static class ProcessTester
+    {
+        public static void Test()
+        {
+            ProcessTst();
+        }
+
+
+        // "console": "internalConsole",  worked with this
+        static void ProcessTst()
+        {
+            // this worked
+            //https://learn.microsoft.com/en-us/answers/questions/204058/how-to-execute-this-command-in-cmd-prompt-using-c
+            var p = new Process
+            {
+                StartInfo =
+                {
+                    Arguments = "/C dir",
+                    FileName = "cmd.exe",
+                }
+            };
+            p.Start();
+            return;
+        }
+    }
+}
